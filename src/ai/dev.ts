cfa75e741196805bcb90b1c253ee701bef4d1a5d
Omar Wairimu <omarwairimu@gmail.com>
2025-09-20 18:26:08 +0000

    check for other potential deployment errors

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -1,7 +1,3 @@
 
 import { config } from 'dotenv';
 config();
-
-import '@/ai/flows/generate-blog-content.ts';
-import '@/ai/flows/recommend-suburbs.ts';
-import '@/ai/flows/submit-contact-form.ts';
